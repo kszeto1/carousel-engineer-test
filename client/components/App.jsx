4cc9520836f2c6e2edd391133873bdf319b30aca
Kenny Szeto <kenny.szeto1@gmail.com>
2019-08-12 00:44:07 -0700

    carousel visible but need to iron out consistency across every element. will choose better pictures and center thumbnail slider.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Carousel } from "react-responsive-carousel";
 
 class App extends React.Component {
   constructor(props) {
@@ -7,7 +8,28 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>Hello World</div>
+      <Carousel>
+        <div>
+          <img src="https://fec-photo-gallery-ks.s3-us-west-1.amazonaws.com/1.jpg" />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src="https://fec-photo-gallery-ks.s3-us-west-1.amazonaws.com/2.jpg" />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src="https://fec-photo-gallery-ks.s3-us-west-1.amazonaws.com/3.jpg" />
+          <p className="legend">Legend 3</p>
+        </div>
+        <div>
+          <img src="https://fec-photo-gallery-ks.s3-us-west-1.amazonaws.com/4.jpg" />
+          <p className="legend">Legend 3</p>
+        </div>
+        <div>
+          <img src="https://fec-photo-gallery-ks.s3-us-west-1.amazonaws.com/5.jpg" />
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
     )
   }
 }
