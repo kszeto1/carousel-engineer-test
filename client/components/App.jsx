234c4bad3daac4f3393c057760fc7f67258e85b0
Kenny Szeto <kenny.szeto1@gmail.com>
2019-08-13 23:26:14 -0700

    linked both carousels. set up for github pages

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,32 +6,41 @@ import '../../public/style.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick(e) {
+    e.preventDefault();
+  }
+
 
   render() {
     return (
-      <Carousel infiniteLoop autoPlay width="700px">
-        <div>
-          <img src="https://avochato-carousel.s3-us-west-1.amazonaws.com/1.jpg" />
-          <p className="legend">Legend 1</p>
-        </div>
-        <div>
-          <img src="https://avochato-carousel.s3-us-west-1.amazonaws.com/2.jpg" />
-          <p className="legend">Legend 2</p>
-        </div>
-        <div>
-          <img src="https://avochato-carousel.s3-us-west-1.amazonaws.com/3.jpg" />
-          <p className="legend">Legend 3</p>
-        </div>
-        <div>
-          <img src="https://avochato-carousel.s3-us-west-1.amazonaws.com/4.jpg" />
-          <p className="legend">Legend 4</p>
-        </div>
-        <div>
-          <img src="https://avochato-carousel.s3-us-west-1.amazonaws.com/5.jpg" />
-          <p className="legend">Legend 5</p>
-        </div>
-      </Carousel>
+      <div>
+        <a href={"../client/components/vanilla-slider/VanillaCarousel.html"}>Go to Vanilla Carousel</a>
+        <Carousel infiniteLoop autoPlay transitionTime={1200} width="700px">
+          <div>
+            <img src="../imagesforslider/1.jpg" />
+            <p className="legend">Avocado so delicious</p>
+          </div>
+          <div>
+            <img src="../imagesforslider/2.jpg" />
+            <p className="legend">We have more avocados</p>
+          </div>
+          <div>
+            <img src="../imagesforslider/3.jpg" />
+            <p className="legend">Don't forget about avocado toast</p>
+          </div>
+          <div>
+            <img src="../imagesforslider/4.jpg" />
+            <p className="legend">Cute avocado plushie</p>
+          </div>
+          <div>
+            <img src="../imagesforslider/5.jpg" />
+            <p className="legend">Nutritious meal</p>
+          </div>
+        </Carousel>
+      </div>
     )
   }
 }
